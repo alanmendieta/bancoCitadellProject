e6cc881dab0ce2e878301994a1e772655804ea6c
Alan Mendieta <alanjpmendieta@gmail.com>
2023-06-12 18:41:21 -0300

    HTML code was added and the web page was styled. An 'Animated On Scroll Library' script was included, and a comment was added in JS.

--- js/alert.js
+++ js/alert.js
@@ -1,24 +1,25 @@
-let mensaje = "Este sitio web es un banco nacional, solo gente mayor de edad puede consultar este apartado.";
-let confirmacion = confirm(mensaje);
+// ALERT MESSAGE PROMPT
+ let message = "Este sitio web es un banco nacional, solo gente mayor de edad puede consultar este apartado.";
+ let confirmation = confirm(message);
 
-if (confirmacion) {
-  let nombre = prompt("Ingrese su nombre:");
-  let apellido = prompt("Ingrese su apellido:");
-  let dni = prompt("Ingrese su DNI:");
-  dni = parseInt(dni);
+ if (confirmation) {
+   let name = prompt("Ingrese su nombre:");
+   let surname = prompt("Ingrese su apellido:");
+   let dni = prompt("Ingrese su DNI:");
+   dni = parseInt(dni);
 
-  let edad = prompt("Por nuestra seguridad ingrese su edad:");
-  edad = parseInt(edad);
+   let age = prompt("Por nuestra seguridad ingrese su edad:");
+   age = parseInt(age);
 
-  if (!isNaN(dni) && !isNaN(edad)) {
-    if (edad >= 18) {
-      alert("Bienvenido al sitio web oficial del Banco Citadell");
-      alert("Por favor, navege con responsabilidad y nunca proporcione datos a ninguna persona.");
-    } else {
-      alert("Siendo menor de edad no puedes acceder a esta sección, busca permiso de tu padre/madre o tutor legal y contáctanos.");
-      window.location.href = "https://www.google.com"
-    }
-  } else {
-    alert("Ingresa números válidos en el DNI y la edad.");
-  }
-}
+   if (!isNaN(dni) && !isNaN(age)) {
+     if (age >= 18) {
+       alert("Bienvenido al sitio web oficial del Banco Citadell");
+       alert("Por favor, navege con responsabilidad y nunca proporcione datos a ninguna persona.");
+     } else {
+       alert("Siendo menor de edad no puedes acceder a esta sección, busca permiso de tu padre/madre o tutor legal y contáctanos.");
+       window.location.href = "https://www.google.com"
+     }
+   } else {
+     alert("Ingresa números válidos en el DNI y la edad.");
+   }
+ }
